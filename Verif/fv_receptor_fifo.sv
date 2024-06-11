da05bbdcc35e3c3a13fdd41e854d2834cc19fcf7
TeresaReLo <joannatere@yahoo.com.mx>
2024-06-11 13:54:22 -0600

    new module

---
+++ Verif/fv_receptor_fifo.sv
@@ -0,0 +1,20 @@
+module fv_fifo#(
+
+);
+
+
+
+/////// Assumptionss//////////////////////
+  
+
+/////// Assertions //////////////////////
+   
+   
+/////// Cover properties/////////////////
+        
+
+
+endmodule
+
+
+bind fifo fv_fifo fv_fifo_inst(.*);
